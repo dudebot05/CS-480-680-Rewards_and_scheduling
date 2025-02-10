21186184946251cebfe71648e752a6cac5384002
aaliyahtasby <164138011+aaliyahtasby@users.noreply.github.com>
2025-02-10 17:29:55 -0600

    Merge branch 'main' of https://github.com/dudebot05/CS-480-680-Rewards_and_scheduling into Test---Aaliyah

--- app/static/register.py
+++ app/static/register.py
@@ -1,15 +1,41 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError, IntegerField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 from ..models.user import User
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Please enter a username: ', validators=[DataRequired(), Length(1, 64), 
-        Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 
-            'Usernames must have only letters, numbers, dots or underscores')])
-    email = StringField('Please enter your email: ', validators=[DataRequired(), Length(1, 64), Email()])
-    password = PasswordField('Please enter a password: ', validators=[DataRequired(), EqualTo('password2', message='Passwords must match')])
-    password2 = PasswordField('Confirm password: ', validators=[DataRequired()])
+    username = StringField(
+        'Please enter a username: ', 
+        validators=[
+            DataRequired(), 
+            Length(1, 64), 
+            Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 
+                   'Usernames must have only letters, numbers, dots or underscores')
+        ]
+    )
+    
+    email = StringField(
+        'Please enter your email: ', 
+        validators=[
+            DataRequired(), 
+            Length(1, 64), 
+            Email()
+        ]
+    )
+    
+    password = PasswordField(
+        'Please enter a password: ', 
+        validators=[
+            DataRequired(), 
+            EqualTo('confirm_password', message='Passwords must match')
+        ]
+    )
+    
+    confirm_password = PasswordField(
+        'Confirm password: ', 
+        validators=[DataRequired()]
+    )
+    
     submit = SubmitField('Create new account')
 
     def validate_email(self, field):
